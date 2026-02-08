4a8408a6d591bb44e78d342e7df0f1cd1bbba607
Prasenjit Chakraborty <148824980+prasenjit-hub@users.noreply.github.com>
2026-02-09 02:08:39 +0530

    Delete components directory

--- components/Chatbot.tsx
+++
@@ -1,8 +0,0 @@
-
-/**
- * DEPRECATED: This component is no longer in use.
- * Replaced by WhatsAppButton.tsx
- */
-import React from 'react';
-const Chatbot: React.FC = () => null;
-export default Chatbot;
